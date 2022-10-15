8f9039a78841e77b7eef53646ad513a9fa618b29
reyno405 <reyno405@gmail.com>
2022-10-15 17:30:21 -0400

    this has a product routes route

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -21,8 +21,24 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData= await  .destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!libraryCardData) {
+      res.status(404).json({ message: 'No library card found with that id!' });
+      return;
+    }
+
+    res.status(200).json(libraryCardData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
